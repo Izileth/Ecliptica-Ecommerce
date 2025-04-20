b2e6933a0b8429767441661f872bde3ec8126fec
Kawã Kleber <kawaklebersc@gmail.com>
2025-04-20 11:46:36 -0300

    Update Routes

--- app/src/data/products/product.tsx
+++ app/src/data/products/product.tsx
@@ -2,19 +2,28 @@ export const DataProducts = [
     {
         id: 1,
         imageUrl:
-        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/a25721b4-ddcc-48c3-bf40-27f77e1e2a1d/Leonardo_Kino_XL_A_model_in_a_clean_studio_with_a_light_beige_3.jpg",
+        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/71e7e50f-d340-4a90-8153-874576dcd1c2/Leonardo_Kino_XL_Fashion_editorial_photography_of_male_and_fem_1.jpg",
         title: "Nova Temporada",
         subtitle: "Descubra as tendências que definirão a próxima estação.",
         buttonText: "Explorar Novidades",
-        navigateTo: "releases",
+        navigateTo: "/coletions/winter",
     },
     {
         id: 2,
         imageUrl:
-        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/a25721b4-ddcc-48c3-bf40-27f77e1e2a1d/Leonardo_Kino_XL_A_model_in_a_clean_studio_with_a_light_beige_2.jpg",
+        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/92842d96-09f4-422e-a894-e04767bbd943/Leonardo_Kino_XL_Studio_editorial_image_of_a_relaxed_male_and_2.jpg",
         title: "Coleção Exclusiva",
         subtitle: "Peças únicas desenvolvidas pelos nossos designers.",
         buttonText: "Ver Exclusivos",
-        navigateTo: "colections/mens",
+        navigateTo: "/colections/mens",
+    },
+    {
+        id: 3,
+        imageUrl:
+        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/ace5fd9d-a650-426e-b3f4-c17314ee8d0f/Leonardo_Kino_XL_Autumninspired_editorial_photo_with_casual_la_3.jpg",
+        title: "Coleção Outono",
+        subtitle: "Peças únicas desenvolvidas pelos nossos designers.",
+        buttonText: "Ver Exclusivos",
+        navigateTo: "/coletions/falls",
     },
 ];
--- app/src/data/static/destacts/destact.ts
+++ app/src/data/static/destacts/destact.ts
@@ -2,7 +2,7 @@ export const DataDestacts = [
     {
         id: 1,
         imageUrl:
-        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/256bf1f3-8863-4193-bbf4-bf6b8e8fe671/Leonardo_Kino_XL_A_woman_in_a_gold_sequined_couture_dress_pose_1.jpg",
+        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/e3fc84c4-78ce-413d-a26d-b1a051eadc04/Leonardo_Kino_XL_Fullbody_fashion_photography_of_a_model_weari_3.jpg",
         title: "Minimalismo Elegante",
         subtitle: "Menos é mais. Descubra nossa coleção de peças minimalistas.",
         buttonText: "Explorar",
@@ -12,7 +12,7 @@ export const DataDestacts = [
     {
         id: 2,
         imageUrl:
-        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/49e276b0-0986-45fb-a26d-e531bd018b76/Leonardo_Phoenix_10_An_elegant_model_poses_in_a_highfashion_st_3.jpg",
+        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/92842d96-09f4-422e-a894-e04767bbd943/Leonardo_Kino_XL_Studio_editorial_image_of_a_relaxed_male_and_3.jpg",
         title: "Essenciais",
         subtitle: "Peças atemporais para um guarda-roupa versátil.",
         buttonText: "Ver Coleção",
@@ -22,7 +22,7 @@ export const DataDestacts = [
     {
         id: 3,
         imageUrl:
-        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/901a7c5b-1874-4511-a798-fee17f5cd58c/Leonardo_Kino_XL_A_young_fashionista_poses_against_a_neon_wall_2.jpg?w=512",
+        "https://cdn.leonardo.ai/users/c60a0145-a4a8-4ee5-91cf-76495889e8b2/generations/840db377-3660-4f9f-bd89-e1592274a6a5/Leonardo_Kino_XL_Spring_collection_photoshoot_in_blooming_gard_1.jpg",
         title: "Nova Coleção",
         subtitle: "Descubra as últimas tendências da estação.",
         buttonText: "Ver Coleção",
